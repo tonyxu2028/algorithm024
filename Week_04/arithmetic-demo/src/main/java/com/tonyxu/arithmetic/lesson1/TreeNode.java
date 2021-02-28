f6d28fb436d6d67943984460daf774f14856b4c3
tonyxu2028 <191284969@qq.com>
2021-02-28 17:20:07 +0800

    add week4

---
+++ Week_04/arithmetic-demo/src/main/java/com/tonyxu/arithmetic/lesson1/TreeNode.java
@@ -0,0 +1,13 @@
+package com.tonyxu.arithmetic.lesson1;
+
+/**
+ * Created on 2020/11/18.
+ *
+ * @author <a href="191284969@qq.com">Tony xu</a>
+ */
+public class TreeNode {
+    int value;
+    TreeNode leftNode;
+    TreeNode rightNode;
+    TreeNode(int x) { value = x; }
+}
